dd4c35e79dcb81fecac694477ec604a31b1f9a0d
WEN_LONG HUANG <whuang148@my.bcit.ca>
2023-02-26 00:34:11 -0800

    made footer copyright into module and updated all pages to reflect changes

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,8 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton() {
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+    console.log($('#navbarPlaceholder').load('/text/nav.html'));
+    console.log($('#footerPlaceholder').load('/text/footer.html'));
+    console.log($('#copyrightPlaceholder').load('/text/copyright.html'));
 }
 loadSkeleton();  //invoke the function
