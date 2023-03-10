910ea1702afe66613a51f5e5ebe1e09d5ec4bd96
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-03-10 11:44:06 -0800

    Add products collection in database; modify code to display products dynamically

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,76 @@ function insertName() {
     });
 }
 insertName(); //run the function
+
+function writeProducts() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var productRef = db.collection("products");
+
+    productRef.add({
+        code: "coffebeans",
+        name: "coffee beans",
+        store: "store1",  // get from database
+        price: "price1", // get from database
+        ingredients: "ingredients1",
+
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+    productRef.add({
+        code: "cake",
+        name: "cake",
+        store: "store2", // get from database
+        price: "price2", // get from database
+        ingredients: "ingredients2",
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2023"))
+    });
+    productRef.add({
+        code: "strawberryjam",
+        name: "strawberry jam",
+        store: "store3",  // get from database
+        price: "price3",  // get from database
+        ingredients: "ingredients3",
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2023"))
+    });
+}
+
+// writeProducts();
+
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("productCardTemplate");
+
+    db.collection(collection).get()   //the collection called "products"
+        .then(allproducts => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each product
+            allproducts.forEach(doc => { //iterate thru each doc
+                var productCode = doc.data().code;    //get unique ID to each product to be used for fetching right image
+                var productName = doc.data().name;       // get value of the "name" key
+                var productStore = doc.data().store;  // get value of the "store" key
+                var productPrice = doc.data().price;  // get value of the "store" key
+                var productIngredients = doc.data().ingredients;  // get value of the "details" key
+                var productTime = doc.data().last_updated;    //get time
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-name').innerHTML = productName;
+                newcard.querySelector('.card-store').innerHTML = productStore;
+                newcard.querySelector('.card-price').innerHTML = productPrice;
+                newcard.querySelector('.card-text').innerHTML = productIngredients;
+                newcard.querySelector('.card-time').innerHTML = productTime;
+                newcard.querySelector('.card-image').src = `./images/${productCode}.jpg`; //Example: cake.jpg
+                newcard.querySelector('a').href = "product.html?docID=" + docID;
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("products");
