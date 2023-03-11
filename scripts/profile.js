f339d5fbdc2184984b463bbf3324d736ef13558d
Vivian Chu <mchu65@my.bcit.ca>
2023-03-10 16:02:16 -0800

    Add save and edit button and connected it with firestore.

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,34 @@
+var currentUser;
+
+function populateUserInfo() {
+    firebase.auth().onAuthStateChanged(user => {// method to check who is the user in firebase
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userSchool = userDoc.data().email;
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("emailInput").value = userEmail;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+        }
+    });
+}
+
 //call the function to run it 
 populateUserInfo();
 
@@ -10,14 +41,15 @@ function saveUserInfo() {
     console.log("inside save UserInfo")
 
     var userName = document.getElementById("nameInput").value;
-    var userEmail = document.getElementById("userEmail").value;
+    // var userEmail = document.getElementById("emailInput").value;
 
-    console.log(userName, userEmail);
+    // console.log(userName, userEmail);
+    console.log(userName);
 
 
     currentUser.update({
         name: userName,
-        email: userEmail
+        // email: userEmail,
     })
         .then(() => {
             console.log("Document successfully updated!");
@@ -25,5 +57,4 @@ function saveUserInfo() {
 
 
     document.getElementById('personalInfoFields').disabled = true;
-
 }
