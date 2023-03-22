4014dd073aaac26abb5dc17cd237e8c2fbe5d335
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-03-22 16:11:06 -0700

    Optimize code for streamlining

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,30 @@
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) { // Will verify who is logged in
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            // console.log(user.displayName);  //print the user name in the browser console
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); //jquery
+                // document.getElementById("name-goes-here").innerText = userName;
+                //method #1:  insert with html only
+                //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+                //method #2:  insert using jquery
+                $(".name-goes-here").text(userName); //using jquery
+            })
+        }
+        // else {
+        //     // No user is signed in.
+        // }
+    });
+}
+insertName(); //run the function
+
 var currentUser;
 
 function populateUserInfo() {
