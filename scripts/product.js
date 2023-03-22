91b24cd3726346d9abebd494e19da6c76f92d306
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-03-22 15:47:04 -0700

    Enable review page submission

--- scripts/product.js
+++ scripts/product.js
@@ -1,16 +1,42 @@
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) { // Will verify who is logged in
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            // console.log(user.displayName);  //print the user name in the browser console
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); //jquery
+                // document.getElementById("name-goes-here").innerText = userName;
+                //method #1:  insert with html only
+                //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+                //method #2:  insert using jquery
+                $(".name-goes-here").text(userName); //using jquery
+            })
+        }
+        // else {
+        //     // No user is signed in.
+        // }
+    });
+}
+insertName(); //run the function
+
 function displayproductInfo() {
     //retrieve the document id from the url
     let params = new URL(window.location.href); //get URL of search bar
     let ID = params.searchParams.get("docID"); //get value for key "id"
     console.log(ID);
 
-    // doublecheck: is your collection called "Reviews" or "reviews"?
     db.collection("products")
         .doc(ID)
         .get()
         .then(doc => {
-            productCode = doc.data().code;
-            productName = doc.data().name;
+            var productCode = doc.data().code;
+            var productName = doc.data().name;
 
             // only populate title, and image
             document.getElementById("productName").innerHTML = productName;
@@ -20,46 +46,56 @@ function displayproductInfo() {
 }
 displayproductInfo();
 
-// function saveProductDocumentIDAndRedirect() {
-//     let params = new URL(window.location.href) //get the url from the search bar
-//     let ID = params.searchParams.get("docID");
-//     localStorage.setItem('productDocID', ID);
-//     window.location.href = 'review.html';
-// }
+function saveProductDocumentIDAndRedirect() {
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('productDocID', ID);
+    window.location.href = 'review.html';
+}
 
-// function populateReviews() {
-//     let productCardTemplate = document.getElementById("reviewCardTemplate");
-//     let productCardGroup = document.getElementById("reviewCardGroup");
+function populateReviews() {
+    let productCardTemplate = document.getElementById("reviewCardTemplate");
+    let productCardGroup = document.getElementById("reviewCardGroup");
+    console.log("inside populate reviews")
 
-//     //let params = new URL(window.location.href) //get the url from the searbar
-//     //let productID = params.searchParams.get("docID");
-//     var productID = localStorage.getItem("productDocID");
+    let params = new URL(window.location.href) //get the url from the searbar
+    let productID = params.searchParams.get("docID");
+    // var productID = localStorage.getItem("productDocID");
+    console.log(productID)
+    db.collection("reviews").where("productID", "==", productID).get()
+        .then(allReviews => {
+            reviews = allReviews.docs;
+            console.log(reviews);
+            reviews.forEach(doc => {
+                var userID = doc.data().userID;
+                console.log(userID)
+                var reviewer = db.collection("users").doc(userID).get().then(
+                    userDoc => {
+                        var userName = userDoc.data().name;
+                        return userName
+                    });
+                console.log(reviewer)
+                var userName = reviewer;
+                var title = doc.data().title;
+                var productRating = doc.data().productRating;
+                var environmentRating = doc.data().environmentRating;
+                var healthRating = doc.data().healthRating;
+                var description = doc.data().description;
+                var rebuyRating = doc.data().rebuyRating;
+                var time = doc.data().timestamp.toDate();
+                console.log(time)
 
-//     // doublecheck: is your collection called "Reviews" or "reviews"?
-//     db.collection("reviews").where("productDocID", "==", productID).get()
-//         .then(allReviews => {
-//             reviews = allReviews.docs;
-//             console.log(reviews);
-//             reviews.forEach(doc => {
-//                 var title = doc.data().title; //gets the name field
-//                 var level = doc.data().level; //gets the unique ID field
-//                 var season = doc.data().season;
-//                 var description = doc.data().description; //gets the length field
-//                 var flooded = doc.data().flooded;
-//                 var scrambled = doc.data().scrambled;
-//                 var time = doc.data().timestamp.toDate();
-//                 console.log(time)
-
-//                 let reviewCard = productCardTemplate.content.cloneNode(true);
-//                 reviewCard.querySelector('.title').innerHTML = title;     //equiv getElementByClassName
-//                 reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();    //equiv getElementByClassName
-//                 reviewCard.querySelector('.level').innerHTML = `level: ${level}`;
-//                 reviewCard.querySelector('.season').innerHTML = `season: ${season}`;
-//                 reviewCard.querySelector('.scrambled').innerHTML = `scrambled: ${scrambled}`;  //equiv getElementByClassName
-//                 reviewCard.querySelector('.flooded').innerHTML = `flooded: ${flooded}`;  //equiv getElementByClassName
-//                 reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
-//                 productCardGroup.appendChild(reviewCard);
-//             })
-//         })
-// }
-// populateReviews();
+                let reviewCard = productCardTemplate.content.cloneNode(true);
+                reviewCard.querySelector('.userName').innerHTML = userName;     //equiv getElementByClassName
+                reviewCard.querySelector('.title').innerHTML = title;     //equiv getElementByClassName
+                reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();    //equiv getElementByClassName
+                reviewCard.querySelector('.productRating').innerHTML = `Overall Rating: ${productRating}`;
+                reviewCard.querySelector('.environmentRating').innerHTML = `Eco-Friendliness: ${environmentRating}`;
+                reviewCard.querySelector('.healthRating').innerHTML = `Healthiness: ${healthRating}`;  //equiv getElementByClassName
+                reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
+                reviewCard.querySelector('.rebuyRating').innerHTML = `Will Buy Again: ${rebuyRating}`;  //equiv getElementByClassName
+                productCardGroup.appendChild(reviewCard);
+            })
+        })
+}
+populateReviews();
---
+++ scripts/review.js
@@ -0,0 +1,93 @@
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) { // Will verify who is logged in
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            // console.log(user.displayName);  //print the user name in the browser console
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); //jquery
+                // document.getElementById("name-goes-here").innerText = userName;
+                //method #1:  insert with html only
+                //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+                //method #2:  insert using jquery
+                $(".name-goes-here").text(userName); //using jquery
+            })
+        }
+        // else {
+        //     // No user is signed in.
+        // }
+    });
+}
+insertName(); //run the function
+
+var productID = localStorage.getItem("productDocID");    //visible to all functions on this page
+console.log(productID);
+
+function getProductName(id) {
+    db.collection("products")
+        .doc(id)
+        .get()
+        .then(doc => {
+            var productName = doc.data().name;
+            document.getElementById("productName").innerHTML = productName; // points to the <h1> element
+        });
+}
+
+getProductName(productID);
+
+function showSliderValue() {
+    var slider = document.getElementById("rebuyRatingSlider");
+    var output = document.getElementById("rebuyRatingValue");
+    output.innerHTML = slider.value; // Display the default slider value
+    
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+    }
+}
+
+showSliderValue();
+
+function writeReview() {
+    console.log("inside write review")
+    let Title = document.getElementById("title").value;
+    let ProductRating = document.getElementById("productRating").value;
+    let EnvironmentRating = document.getElementById("environmentRating").value;
+    let HealthRating = document.getElementById("healthRating").value;
+    let Description = document.getElementById("description").value;
+    let RebuyRating = document.getElementById("rebuyRatingSlider").value;
+    console.log(Title, ProductRating, EnvironmentRating, HealthRating, Description, RebuyRating);
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    // var userEmail = userDoc.data().email;
+                    db.collection("reviews").add({
+                        productID: productID,
+                        userID: userID,
+                        title: Title,
+                        productRating: ProductRating,
+                        environmentRating: EnvironmentRating,
+                        healthRating: HealthRating,
+                        description: Description,
+                        rebuyRating: RebuyRating,
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                    }).then(() => {
+                        window.location.href = "product.html?docID=" + productID;
+                    })
+                })
+        } else {
+            console.log("No user is signed in");
+            window.location.href = 'index.html'; // redirect to login page
+        }
+    });
+}
