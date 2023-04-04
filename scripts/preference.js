592d460f5415e34a2e33ed499f8417a35324c0c7
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-04-03 18:01:24 -0700

    Implement functional preference list

--- scripts/preference.js
+++ scripts/preference.js
@@ -1,26 +1,114 @@
-function insertName() {
+var currentUser;
+
+function doAll() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
-        if (user) { // Will verify who is logged in
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            // console.log(user.displayName);  //print the user name in the browser console
-            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-            currentUser.get().then(userDoc => {
-                //get the user name
-                var userName = userDoc.data().name;
-                console.log(userName);
-                //$("#name-goes-here").text(userName); //jquery
-                // document.getElementById("name-goes-here").innerText = userName;
-                //method #1:  insert with html only
-                //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-                //method #2:  insert using jquery
-                $(".name-goes-here").text(userName); //using jquery
-            })
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid);
+            console.log(currentUser);
+
+            insertName();
+            displayPreferences("preferencelist");
+        } else {
+            console.log("No user is signed in.")
+            window.location.href = "login.html";
         }
-        // else {
-        //     // No user is signed in.
-        // }
     });
 }
-insertName(); //run the function
+
+doAll();
+
+function insertName() {
+    currentUser.get().then(userDoc => {
+        var userName = userDoc.data().name;
+        console.log(userName);
+        $(".name-goes-here").text(userName); //using jquery
+    })
+}
+// insertName(); //run the function
+
+
+function writePreferences() {
+    var preferenceList = db.collection("preferencelist");
+    preferenceList.doc("sugar").set({});
+    preferenceList.doc("flour").set({});
+    preferenceList.doc("eggs").set({});
+    preferenceList.doc("butter").set({});
+    preferenceList.doc("vanilla").set({});
+    preferenceList.doc("baking powder").set({});
+    preferenceList.doc("salt").set({});
+    preferenceList.doc("cherries").set({});
+    preferenceList.doc("milk").set({});
+    preferenceList.doc("soy").set({});
+    preferenceList.doc("wheat").set({});
+    preferenceList.doc("xanthan gum").set({});
+    preferenceList.doc("artificial flavor").set({});
+    preferenceList.doc("artificial color").set({});
+    preferenceList.doc("coffee beans").set({});
+    preferenceList.doc("preservatives").set({});
+    preferenceList.doc("strawberries").set({});
+    preferenceList.doc("pectin").set({});
+    preferenceList.doc("paper").set({});
+    preferenceList.doc("polyethylene terephthalate(PET) plastic").set({});
+    preferenceList.doc("glass").set({});
+    preferenceList.doc("nylon").set({});
+}
+
+// writePreferences();
+
+function displayPreferences(collection) {
+    let cardTemplate = document.getElementById("preferenceCardTemplate")
+    db.collection(collection).get()
+        .then(allPreferences => {
+            allPreferences.forEach(doc => {
+                var preference = doc.id;
+                console.log(preference)
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                newcard.querySelector(".card-title").innerHTML = preference;
+
+                newcard.querySelector('i').id = 'update-' + preference;
+                newcard.querySelector('i').onclick = () => updatePreferences(preference);
+
+                currentUser.get().then(userDoc => {
+                    var preferences = userDoc.data().preferences;
+                    console.log(preferences)
+                    if (preferences.includes(preference)) {
+                        document.getElementById('update-' + preference).innerText = 'cancel';
+                    }
+                })
+
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+            })
+        })
+}
+
+function updatePreferences(preference) {
+    console.log("clicked!")
+    currentUser.get().then(userDoc => {
+        var preferences = userDoc.data().preferences;
+        if (preferences.includes(preference)) {
+            currentUser.update({
+                preferences: firebase.firestore.FieldValue.arrayRemove(preference)
+            }, {
+                merge: true
+            })
+                .then(function () {
+                    console.log("preference has been removed for: " + currentUser);
+                    var iconID = 'update-' + preference;
+                    document.getElementById(iconID).innerText = 'check_circle_outline';
+                });
+        } else {
+            currentUser.set({
+                preferences: firebase.firestore.FieldValue.arrayUnion(preference)
+            }, {
+                merge: true
+            })
+                .then(function () {
+                    console.log("preference has been saved for: " + currentUser);
+                    var iconID = 'update-' + preference;
+                    document.getElementById(iconID).innerText = 'cancel';
+                });
+        }
+    })
+}
